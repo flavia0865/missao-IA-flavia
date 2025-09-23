50ddb33f154bf9f21aee834d70859f8172428398
flavia0865 <160143425+flavia0865@users.noreply.github.com>
2025-09-23 14:16:03 -0300

    23/09

---
+++ main.js
@@ -0,0 +1,73 @@
+
+
+
+
+
+
+const perguntas = [
+    {
+        enunciado: "Pergunta?",
+        alternativas: [
+            {
+                texto: " Texto/resposta",
+                afirmacao: " Afirmação ",
+            },
+            {
+                texto: " Texto/resposta ",
+                afirmacao: " Afirmação ",
+            }
+        ]
+    },
+    {
+        enunciado: " Pergunta?",
+        alternativas: [
+            {
+                texto: " Texto/resposta ",
+                afirmacao: " Afirmação ",
+            },
+            {
+                texto: " Texto/resposta ",
+                afirmacao: " Afirmação ",
+            }
+        ]
+    },
+    {
+        enunciado: " Pergunta?",
+        alternativas: [
+            {
+                texto: " Texto/resposta ",
+                afirmacao: " Afirmação ",
+            },
+            {
+                texto: " Texto/resposta ",
+                afirmacao: " Afirmação ",
+            }
+        ]
+    },
+    {
+        enunciado: " Pergunta?",
+        alternativas: [
+            {
+                texto: " Texto/resposta ",
+                afirmacao: " Afirmação ",
+            },
+            {
+                texto: " Texto/resposta ",
+                afirmacao: " Afirmação ",
+            }
+        ]
+    },
+    {
+        enunciado: " Pergunta?",
+        alternativas: [
+            {
+                texto: " Texto/resposta ",
+                afirmacao: "Afirmação",
+            },
+            {
+                texto: " Texto/resposta ",
+                afirmacao: " Afirmação ",
+            }
+        ]
+    },
+];
