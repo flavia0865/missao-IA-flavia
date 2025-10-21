9314550066d97239f9a158c797891fc94b020127
flavia0865 <160143425+flavia0865@users.noreply.github.com>
2025-10-21 14:06:11 -0300

    21/10

--- main.js
+++ main.js
@@ -106,7 +106,7 @@ function mostraAlternativas(){
         const botaoAlternativas = document.createElement("button");
         botaoAlternativas.textContent = alternativa.texto;
         botaoAlternativas.addEventListener('click', function(){
-            ataul++;
+            atual++;
             mostraPergunta();
         })
         caixaAlternativas.appendChild(botaoAlternativas);
