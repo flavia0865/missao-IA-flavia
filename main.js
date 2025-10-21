4e1983a31050475a3ac1da34fc701cbcc25c3064
flavia0865 <160143425+flavia0865@users.noreply.github.com>
2025-10-21 14:26:22 -0300

    21/10

--- main.js
+++ main.js
@@ -94,23 +94,21 @@ const perguntas = [
 
 let atual = 0;
 let perguntaAtual;
+let histotiaFinal = " ";
 
-function mostraPergunta(){
+function mostraPergunta() {
     perguntaAtual = perguntas[atual];
     caixaPerguntas.textContent = perguntaAtual.enunciado;
     mostraAlternativas();
 }
 
-function mostraAlternativas(){
-    for (const alternativa of perguntaAtual.alternativas){
+function mostraAlternativas() {
+    for (const alternativa of perguntaAtual.alternativas) {
         const botaoAlternativas = document.createElement("button");
         botaoAlternativas.textContent = alternativa.texto;
-        botaoAlternativas.addEventListener('click', function(){
-            atual++;
-            mostraPergunta();
-        })
+        botaoAlternativas.addEventListener('click', () => respostaSelecionada(alternativa))
         caixaAlternativas.appendChild(botaoAlternativas);
-        
+
     }
 }
 
